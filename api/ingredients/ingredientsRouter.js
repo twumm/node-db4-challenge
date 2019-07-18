0bc14a549051013a1e4c9a510a50989737c063c4
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-18 14:38:45 +0000

    feature: Create base setup for ingredients router and model

---
+++ api/ingredients/ingredientsRouter.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const IngredientsDb = require('./ingredientsModel');
+const router = express.Router();
+
+router.get('/:id/recipes', async (req, res, next) => {
+  
+})
+
+module.exports = router;
