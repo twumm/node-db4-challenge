10ce4d0d0e56d00622c5b93cfa0a072e357afea8
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-18 11:54:57 +0000

    feature: Add logger

--- api/server.js
+++ api/server.js
@@ -3,5 +3,13 @@ const express = require('express');
 const server = express();
 
 server.use(express.json());
+server.use(logger);
+
+function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} request from ${req.url}`
+  );
+  next();
+}
 
 module.exports = server;
