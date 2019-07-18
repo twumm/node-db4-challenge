910aa1d1e9f5e5a44f83887812c6c12c8e4cba3d
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-18 14:08:59 +0000

    feature: Add method to get all recipes

--- api/recipes/recipesRouter.js
+++ api/recipes/recipesRouter.js
@@ -3,4 +3,13 @@ const express = require('express');
 const recipesDb = require('./recipesModel');
 const router = express.Router();
 
+router.get('/', async (req, res, next) => {
+  try {
+    const recipes = await recipesDb.getRecipes();
+    res.status(200).json(recipes);
+  } catch (error) {
+    next(new Error('We are unable to retrieve recipes. Kindly try again.'))
+  }
+})
+
 module.exports = router;
